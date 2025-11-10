15c5957045435700b7828a84bb2b2f3bbc2477a0
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-11-10 13:37:17 +0100

    Ignore BaseModel core-schema warning globally

--- pydantic_fixturegen/__init__.py
+++ pydantic_fixturegen/__init__.py
@@ -1,7 +1,17 @@
 """Top-level package for pydantic-fixturegen."""
 
+from __future__ import annotations
+
+import warnings
+
 from .core.version import get_tool_version
 
 __all__ = ["__version__", "get_tool_version"]
 
 __version__ = get_tool_version()
+
+warnings.filterwarnings(
+    "ignore",
+    message=(r"The `__get_pydantic_core_schema__` method of the `BaseModel` class is deprecated\."),
+    category=DeprecationWarning,
+)
